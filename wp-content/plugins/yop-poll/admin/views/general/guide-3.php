ebccbff22603d4337a67fb9aee1a3570211b0653
perel55 <bojoferryljay@gmail.com>
2024-09-17 12:15:56 +0800

    poll added

---
+++ wp-content/plugins/yop-poll/admin/views/general/guide-3.php
@@ -0,0 +1,57 @@
+<div class="row guide-section-header">
+	<div class="col-md-12 text-center">
+		<h3 class="main-headline">
+			"Boost your engagement and revenue with this <u>FREE</u> guide - usually $37, now yours at No Cost!"
+		</h3>
+	</div>
+</div>
+<div class="row guide-section-cover">
+	<div class="col-md-12 text-center">
+		<img src="<?php echo esc_url( YOP_POLL_URL ) . '/admin/assets/images/guide.png'; ?>">
+	</div>
+</div>
+<div class="row guide-section-header">
+	<div class="col-md-12 text-center yop-poll-guide-subheadline">
+		Get instant access to <b>Revenue - Boosting Polls: 10 Innovative Ways Creators Are Using Polls to Engage and Sell</b> - a must-have guide 
+		filled with powerful strategies that top creators use to skyrocket their engagement and maximize profits.
+	</div>
+</div>
+<div class="row guide-section">
+	<div class="col-md-12 text-left yop-poll-guide-text">
+		<ul>
+			<li>
+				<b>Increase conversions:</b> Learn how to craft polls that drive more sales and leads
+			</li>
+			<li>
+				<b>Save time:</b> Implement proven strategies without trial and error
+			</li>
+			<li>
+				<b>Boost engagement:</b> Discover techniques that keep your audience hooked and participating
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="row guide-section">
+	<div class="col-md-12 text-center yop-poll-guide-text">
+		Enter your best email to get your <u>FREE</u> guide now!
+	</div>
+</div> 
+<div class="row guide-section">
+	<div class="col-md-2 email-container text-center">
+	</div>
+	<div class="col-md-8 email-container text-center">
+		<input type="text" class="guide-email form-control input-lg" placeholder="Email Address">
+	</div>
+	<div class="col-md-2 email-container text-center">
+	</div>
+</div>
+<div class="row guide-section">
+	<div class="col-md-12 email-container text-center">
+		<a class="btn send-poll-guide" href="#" target="_blank">Download Your Free Copy Now!</a>
+	</div>
+</div>
+<div class="row guide-section">
+	<div class="col-md-12 text-left guide-section-never-show">
+		<a href="#" class="stop-showing-guide">Don't show this again</a>
+	</div>
+</div>
